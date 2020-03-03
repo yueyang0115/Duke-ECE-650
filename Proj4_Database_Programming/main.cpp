86bd96f5e5ffe704c0b4a88a61803798e9a7e4e6
yy <yueyang0115@outlook.com>
2020-03-02 23:20:16 -0500

    add create table

--- Proj4_Database_Programming/main.cpp
+++ Proj4_Database_Programming/main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <pqxx/pqxx>
-
+#include <fstream>
+#include <string>
 #include "exerciser.h"
 
 using namespace std;
 using namespace pqxx;
 
+void createTable(string file_name, connection *C){
+  string sql;
+  string line;
+  ifstream myfile (file_name.c_str());
+  if (myfile.is_open()){
+      while(getline(myfile,line)){
+      	  sql.append(line);
+      }
+       myfile.close();
+  }
+  else{
+    cout << "Unable to open file"<<endl;
+    return;
+  }
+  work W(*C);
+  W.exec( sql );
+  W.commit();
+  cout << "Table created successfully" << endl;
+}
+
 int main (int argc, char *argv[]) 
 {
-
   //Allocate & initialize a Postgres connection object
   connection *C;
-
   try{
     //Establish a connection to the database
-    //Parameters: database name, user name, user password
     C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd");
     if (C->is_open()) {
       cout << "Opened database successfully: " << C->dbname() << endl;
@@ -27,17 +45,13 @@ int main (int argc, char *argv[])
     return 1;
   }
 
-
   //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
   //      load each table with rows from the provided source txt files
-
-
-  exercise(C);
-
+  createTable("tables.sql",C);
+  //exercise(C);
 
   //Close database connection
   C->disconnect();
-
   return 0;
 }
 
