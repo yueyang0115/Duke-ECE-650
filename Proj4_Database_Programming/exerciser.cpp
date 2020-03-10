7e64af7c9f175ffe62960c3e3d1c2eddd192332d
yy <yueyang0115@outlook.com>
2020-03-09 23:53:01 -0400

    add all query

--- Proj4_Database_Programming/exerciser.cpp
+++ Proj4_Database_Programming/exerciser.cpp
@@ -3,4 +3,27 @@
 void exercise(connection *C)
 {
   query1(C, 1, 35, 40, 0, 0, 0, 0, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  query1(C, 0, 35, 40, 0, 0, 0, 1, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  query1(C, 0, 35, 40, 0, 0, 0, 0, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  query2(C, "Orange");
+  query3(C, "FloridaState");
+  query3(C, "Duke");
+  query4(C, "NC", "Red");
+  query4(C, "NC", "DarkBlue");
+  query5(C, 13);
+  add_player(C, 1, 1, "yyyy", "xxxx", 1, 1, 1, 1, 1, 1);
+  query1(C, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  query2(C, "b");
+  query3(C, "haha");
+  query4(C, "NC", "Red");
+  query5(C, 5);
+  add_player(C, 1, 60, "xxxxx", "yyyy", 20, 20, 10, 10, 5.3, 5.3);
+  add_team(C, "team_baldwin", 10, 3, 20, 0);
+  add_state(C, "N/A");
+  add_color(C, "perry-winkle");
+  query1(C, 1, 10, 30, 0, 0, 40, 0, 0, 6, 0, 0, 5, 0, 0.0, 10.0, 0, 0.0, 10.0);
+  query2(C, "Gold");
+  query3(C, "FloridaState");
+  query4(C, "FL", "Maroon");
+  query5(C, 8);
 }
--- Proj4_Database_Programming/query_funcs.cpp
+++ Proj4_Database_Programming/query_funcs.cpp
@@ -14,7 +14,6 @@ void add_player(connection *C, int team_id, int jersey_num, string first_name, s
   W.commit();
 }
 
-
 void add_team(connection *C, string name, int state_id, int color_id, int wins, int losses)
 {
   work W(*C); 
@@ -39,7 +38,6 @@ void add_color(connection *C, string name)
   W.exec(sql);
   W.commit();
 }
-
 
 void query1(connection *C,
 	    int use_mpg, int min_mpg, int max_mpg,
@@ -75,29 +73,66 @@ void query1(connection *C,
   result R(N.exec(sql.str()));
   cout << "PLAYER_ID TEAM_ID UNIFORM_NUM FIRST_NAME LAST_NAME MPG PPG RPG APG SPG BPG" << endl;
   for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-    cout << c[0].as<int>() << " " << c[1].as<int>() << " " << c[2].as<int>() << " " <<
-      c[3].as<string>() << " " << c[4].as<string>() << " " << c[5].as<int>() << " " <<
-      c[6].as<int>() << " " << c[7].as<int>() << " " << c[8].as<int>() << " " <<
-      c[9].as<double>() << " " << c[10].as<double>() << endl;
+    cout << c[0].as<int>() << " " << c[1].as<int>() << " " << c[2].as<int>() << " " << c[3].as<string>() << " " << c[4].as<string>() << " " << c[5].as<int>() << " " << c[6].as<int>() << " " << c[7].as<int>() << " " << c[8].as<int>() << " " << c[9].as<double>() << " " << c[10].as<double>() << endl;
   }
 }
 
-
 void query2(connection *C, string team_color)
 {
+  work W(*C);
+  stringstream sql;
+  sql << "SELECT TEAM.NAME FROM TEAM, COLOR WHERE TEAM.COLOR_ID = COLOR.COLOR_ID AND COLOR.NAME = " << W.quote(team_color) << ";";
+  W.commit();
+  nontransaction N(*C);
+  result R(N.exec(sql.str()));
+  cout << "NAME" << endl;
+  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+    cout << c[0].as<string>() <<endl;
+  }
 }
 
 
 void query3(connection *C, string team_name)
 {
+  work W(*C);
+  stringstream sql;
+  sql << "SELECT FIRST_NAME, LAST_NAME FROM PLAYER, TEAM WHERE PLAYER.TEAM_ID = TEAM.TEAM_ID AND TEAM.NAME = " << W.quote(team_name) << " ORDER BY PPG DESC;";
+  W.commit();
+  nontransaction N(*C);
+  result R(N.exec(sql.str()));
+  cout << "FIRST_NAME LAST_NAME" << endl;
+  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+    cout << c[0].as<string>() << " "  << c[1].as<string>() << endl;
+  }
 }
-
 
 void query4(connection *C, string team_state, string team_color)
 {
+  work W(*C);
+  stringstream sql;
+  sql << "SELECT FIRST_NAME, LAST_NAME, UNIFORM_NUM FROM PLAYER, STATE, COLOR, TEAM WHERE ";
+  sql << "PLAYER.TEAM_ID = TEAM.TEAM_ID AND TEAM.COLOR_ID = COLOR.COLOR_ID AND TEAM.STATE_ID = STATE.STATE_ID AND ";
+  sql << "STATE.NAME = " << W.quote(team_state) << " AND " << "COLOR.NAME = " << W.quote(team_color) << ";";
+  W.commit();
+  nontransaction N(*C);
+  result R(N.exec(sql.str()));
+  cout << "FIRST_NAME LAST_NAME UNIFORM_NUM" << endl;
+  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+    cout << c[0].as<string>() << " " << c[1].as<string>() << " " << c[2].as<int>() <<endl;
+  }
 }
-
 
 void query5(connection *C, int num_wins)
 {
+  work W(*C);
+  stringstream sql;
+  sql << "SELECT FIRST_NAME, LAST_NAME, NAME, WINS FROM PLAYER, TEAM WHERE ";
+  sql << "PLAYER.TEAM_ID = TEAM.TEAM_ID AND WINS >" << num_wins << ";";
+  W.commit();
+  nontransaction N(*C);
+  result R(N.exec(sql.str()));
+  cout << "FIRST_NAME LAST_NAME UNIFORM_NUM" << endl;
+  for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+    cout << c[0].as<string>() << " " << c[1].as<string>() << " " << c[2].as<string>() << " " << c[3].as<int>() <<endl;
+  }
 }
