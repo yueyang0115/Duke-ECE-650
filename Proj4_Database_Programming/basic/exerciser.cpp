81d725637936b39e5744e3f173d9f1192f3faa13
yy258 <yy258@duke.edu>
2020-03-16 02:43:20 -0400

    add mysite directory, change query4 to raw query, print out result success

--- Proj4_Database_Programming/basic/exerciser.cpp
+++ Proj4_Database_Programming/basic/exerciser.cpp
@@ -4,14 +4,11 @@ void exercise(connection * C) {
   query1(C, 1, 35, 40, 0, 0, 0, 0, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   query1(C, 0, 35, 40, 0, 0, 0, 1, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   query2(C, "Green");
-  query3(C, "Florida");
   query3(C, "Duke");
-  query4(C, "NC", "Blue");
+  query4(C, "MA", "Maroon");
   query5(C, 13);
-  query4(C, "NC", "Blue");
-  query5(C, 5);
   add_player(C, 1, 60, "aaa", "bbb", 20, 20, 10, 10, 5, 5);
   add_team(C, "myteam", 10, 3, 20, 0);
   add_state(C, "N/A");
-  add_color(C, "Black");
+  add_color(C, "Pink");
 }
---
+++ Proj4_Database_Programming/mysite/acc_bball/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AccBballConfig(AppConfig):
+    name = 'acc_bball'
